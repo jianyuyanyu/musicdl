742c5b9e694ee7de8d95b7eb702ad0c2cf2800c0
CharlesPikachu <blwx96@connect.hku.hk>
2026-04-13 19:47:36 +0800

    refactor migu and youtube music client, fix several bugs

--- musicdl/modules/sources/joox.py
+++ musicdl/modules/sources/joox.py
@@ -54,7 +54,7 @@ class JooxMusicClient(BaseMusicClient):
         # init
         song_info, request_overrides, song_info_flac = SongInfo(source=self.source), request_overrides or {}, song_info_flac or SongInfo(source=self.source)
         if (not isinstance(search_result, dict)) or (not (song_id := search_result.get('id'))): return song_info
-        # obtain basic song_info
+        # parse download url based on arguments
         if lossless_quality_is_sufficient and song_info_flac.with_valid_download_url and (song_info_flac.ext in lossless_quality_definitions): song_info = song_info_flac
         else:
             (resp := self.get('https://api.joox.com/web-fcgi-bin/web_get_songinfo', params={'songid': song_id, 'lang': lang, 'country': country}, **request_overrides)).raise_for_status()
